f4e2d0dbd6ab924da8bd02b3a8d52a2c04dee8d6
FaintNutria4 <55836785+FaintNutria4@users.noreply.github.com>
2023-11-09 18:34:09 +0100

    Update I_Blockchain.java

--- privato-do/src/main/resources/Interfaces/I_Blockchain.java
+++ privato-do/src/main/resources/Interfaces/I_Blockchain.java
@@ -1,14 +1,14 @@
 package Interfaces;
 
-public class I_Blockchain {
+interface I_Blockchain {
     /** Adds new block to blockchain */
-  addBlock(block: Block): Block
+  Public I_Block addBlock(I_Block block);
   /**
    * Validates the chain by checking if:
    * - every element's last hash value matches previous block's hash
    * - data hasn't been tampered (which will produce a different hash value)
    */
-  isValid(blockchain: Blockchain): boolean
+  Public Boolean isValid(I_Blockchain blockchain);
   /** The new blockchain that is a candidate for replacing the current blockchain */
-  replace(blockchain: Blockchain): boolean
+  Public Boolean replace(I_Blockchain blockchain );
 }
